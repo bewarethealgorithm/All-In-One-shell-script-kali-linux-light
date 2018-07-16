e8557b655ce8384ba5f55dc39a5acb991acf9ea2
bewarethealgorithm <37989051+bewarethealgorithm@users.noreply.github.com>
2018-07-15 22:34:38 -0400

    Add files via upload

---
+++ All-In-One-Part-One-min20min.sh
@@ -0,0 +1,18 @@
+sudo apt-get install festival -y && apt-get install pulseaudio -y && 
+sudo apt install figlet -y && 
+figlet Hello World && systemctl --user enable pulseaudio && 
+echo "Hello world.. d alt pirate at your service. this is an all in one script to update to upgrade and to clone a few extras. just take a look in your applications after your pc restarts. oh and by the way this script is intended to be used with light versions of kali linux.. use this after a fresh install. this script should take about 20 minutes or so depending on your internet speed.. so good luck folks and good day.. bye now" > TossMe && festival --tts TossMe && sudo apt-get install kismet -y && sudo apt-cache search kali-linux && sudo apt-get install kali-desktop-gnome -y && sudo apt-get install kali-linux-wireless -y && sudo apt-get install kali-linux-pwtools -y && sudo apt-get install kali-linux-forensic -y && sudo apt-get install tor -y && sudo apt-get install bleachbit -y && sudo apt-get install clamav -y && sudo apt get install virtualbox-dkms -y && sudo apt-get install virtualbox -y && sudo apt-get install git -y && sudo apt-get install gdebi -y && apt-get install qbittorrent -y && sudo apt-get install armitage -y && sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install leafpad -y && sudo apt-get install synaptic -y && sudo apt-get install kdenlive -y && sudo apt-get install alacarte -y && sudo apt-get install xine-ui -y && sudo apt-get install vlc -y && sudo apt-get update -y && sudo apt-get install terminator -y && sudo apt-get install python -y && sudo apt-get install python2 -y && sudo apt-get install python3 -y && sudo apt-get install isc-dhcp-server -y && sudo apt-get install dsniff -y && sudo apt-get install hostapd -y && sudo apt-get install sslstrip -y && sudo apt-get install beef-xss -y && sudo apt-get install lighttpd -y && sudo apt-get install ettercap-graphical -y && sudo apt-get install bettercap -y && sudo apt-get install openvas -y && echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | sudo tee /etc/apt/sources.list.d/etcher.list && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61 && sudo apt-get update -y && sudo apt-get install etcher-electron -y &&
+git clone 
+git clone https://github.com/Bhai4You/Kali-Linux.git
+git clone https://github.com/AresS31/wirespy.git
+git clone https://github.com/Rajkumrdusad/Tool-X.git
+git clone https://github.com/FluxionNetwork/fluxion.git 
+git clone https://github.com/joker25000/DZGEN.git
+git clone https://github.com/PowerScript/KatanaFramework.git
+git clone https://github.com/P0cL4bs/WiFi-Pumpkin.git 
+git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git 
+git clone https://github.com/misterch0c/shadowbroker.git
+git clone https://github.com/thehackingsage/hacktronian.git
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install libreoffice -y && sudo apt-get install kali-linux && sudo apt autoclean -y && sudo apt autoremove -y && sudo apt dist-upgrade -y && sudo apt update -y && sudo apt upgrade -y && bleachbit --list | grep -E "[a-z0-9_\-]+\.[a-z0-9_\-]+" | grep -v system.free_disk_space | xargs bleachbit --clean && sudo reboot
+
+
